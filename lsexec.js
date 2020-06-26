ffbc77ce2d72f45ff3880bce268be3d1e2cb59a7
Jag Shetty <59068693+jagcf@users.noreply.github.com>
2020-06-26 16:27:46 -0700

    Update lsexec.js

--- lsexec.js
+++ lsexec.js
@@ -1,13 +1,36 @@
 const { exec } = require("child_process");
 
-exec("ls -la", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+
+(async () => {
+
+    exec("ls -la", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+
+
+    exec(" npx lerna ls --toposort --include-merged-tags --include-dependents --ignore '@dev-tool/*' --json --since", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+   
+
+})().catch(err => {
+    console.error(err);
+    process.exit(-1);
 });
